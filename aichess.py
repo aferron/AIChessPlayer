6ec8efb172f0f2dbd70212d7d333928457f02407
Amila Ferron <aferron@pdx.edu>
2022-11-11 21:50:32 -0800

    Create Game class

--- aichess.py
+++ aichess.py
@@ -1,54 +1,35 @@
+from aichessboard import AIChessBoard
 import chess
 from chess import Move
 
-class CustomBoard(chess.Board):
-    def outcome(self):
-        black_pieces = self.occupied_co[chess.BLACK]
-        white_pieces = self.occupied_co[chess.WHITE]
-        black_wins = black_pieces & chess.BB_BACKRANKS != 0
-        white_wins = white_pieces & chess.BB_BACKRANKS != 0
-        
-        if black_wins:
-            return chess.Outcome(8, 0)
-        elif white_wins:
-            return chess.Outcome(8, 1)
-        else:
-            return None
+class Game:
+    def __init__(self) -> None:
+        self.board = AIChessBoard('8/pppppppp/8/8/8/8/PPPPPPPP/8')
 
-    def print_binary_examples(self):
-        print()
-        print("white pieces: {0:b}".format(self.occupied_co[chess.WHITE]))
-        print("black pieces: {0:b}".format(self.occupied_co[chess.BLACK]))
-        print("backranks: {0:b}".format(chess.BB_BACKRANKS))
-        print("pawns: {0:b}".format(self.pawns))
-        print("pawns & backranks: {0:b}".format(self.pawns & chess.BB_BACKRANKS))
+    def __make_a_move(self, move_from: chess.Square, move_to: chess.Square) -> None:
+        self.board.push(Move(move_from, move_to))
+        print(self.board, "\n")
+        outcome = self.board.outcome()
+        if outcome != None:
+            print("white" if outcome.winner else "black", " wins")
 
+    def run_example_game(self) -> None:
+        print("start game\n", self.board, "\n")
+        self.__make_a_move(chess.A2, chess.A4)
+        self.__make_a_move(chess.B7, chess.B5)
+        self.__make_a_move(chess.A4, chess.B5)
+        self.__make_a_move(chess.A7, chess.A5)
+        self.__make_a_move(chess.B5, chess.B6)
+        self.__make_a_move(chess.C7, chess.B6)
+        self.__make_a_move(chess.E2, chess.E4)
+        self.__make_a_move(chess.A5, chess.A4)
+        self.__make_a_move(chess.B2, chess.B4)
+        self.__make_a_move(chess.A4, chess.A3)
+        self.__make_a_move(chess.E4, chess.E5)
+        self.__make_a_move(chess.A3, chess.A2)
+        self.__make_a_move(chess.F2, chess.F4)
+        self.__make_a_move(chess.A2, chess.A1)
 
-board = CustomBoard('8/pppppppp/8/8/8/8/PPPPPPPP/8')
-print("start game\n", board, "\n")
-
-def make_a_move(move_from: chess.Square, move_to: chess.Square):
-    board.push(Move(move_from, move_to))
-    print(board, "\n")
-    outcome = board.outcome()
-    if outcome != None:
-        print("white" if outcome.winner else "black", " wins")
-
-def run_example_game():
-    make_a_move(chess.A2, chess.A4)
-    make_a_move(chess.B7, chess.B5)
-    make_a_move(chess.A4, chess.B5)
-    make_a_move(chess.A7, chess.A5)
-    make_a_move(chess.B5, chess.B6)
-    make_a_move(chess.C7, chess.B6)
-    make_a_move(chess.E2, chess.E4)
-    make_a_move(chess.A5, chess.A4)
-    make_a_move(chess.B2, chess.B4)
-    make_a_move(chess.A4, chess.A3)
-    make_a_move(chess.E4, chess.E5)
-    make_a_move(chess.A3, chess.A2)
-    make_a_move(chess.F2, chess.F4)
-    make_a_move(chess.A2, chess.A1)
-
-run_example_game()
-board.print_binary_examples()
+game = Game()
+game.run_example_game()
+game.board.print_binary_examples()
